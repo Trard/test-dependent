d0773b83a2cf690361f38d8ba1ff96cb95b8da0d
Trard <rivaavir3@gmail.com>
2021-06-17 17:24:51 +0300

    added_and_used_prettier

--- src/random.js
+++ src/random.js
@@ -1,9 +1,9 @@
 const get_random_size_array = (limit) => {
     let array = [];
-    
-    for ( let i = 0; i < limit; i = i + Math.random() ) {
+
+    for (let i = 0; i < limit; i = i + Math.random()) {
         array.push("123");
-    };
+    }
 
     return array;
 };
