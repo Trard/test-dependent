2b2ff01e805bbbcf3e838b099a95da69147dcbba
Trard <rivaavir3@gmail.com>
2021-06-22 16:39:53 +0300

    new test

--- src/random.js
+++ src/random.js
@@ -1,8 +1,16 @@
-const get_random_size_array = (limit) => {
+'use strict'
+
+const getRandomIntInclusive = (min, max) => {
+    let min_ceil = Math.ceil(min);
+    let max_floor = Math.floor(max);
+    return Math.floor(Math.random() * (max_floor - min_ceil + 1)) + min_ceil;
+}
+
+const get_random_size_array = (min, max) => {
     let array = [];
 
-    for (let i = 0; i < limit; i = i + Math.random()) {
-        array.push("123");
+    for (let i = 0; i < getRandomIntInclusive(min, max); i++) {
+        array.push("123")
     }
 
     return array;
