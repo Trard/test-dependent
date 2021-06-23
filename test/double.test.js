a0f7438a524c3b21d8985e8d6f4a3253e6270c25
Trard <rivaavir3@gmail.com>
2021-06-23 15:56:40 +0300

    fix?

--- test/double.test.js
+++ test/double.test.js
@@ -4,5 +4,5 @@ test('size of doubled size array', () => {
     let test_array = [1, 2, 3];
 
     //size array
-    expect(double(test_array)).tohavelengthnumber(test_array.length * 2);
+    expect(double(test_array)).toHaveLength(test_array.length * 2);
 });
